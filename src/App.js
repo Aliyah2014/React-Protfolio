3bbcce86384adbba8103b1400e93ee0547b680d6
Aliyah2014 <86959592+Aliyah2014@users.noreply.github.com>
2023-03-14 13:47:30 +0000

    Pages code added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Contacts from './pages/Contact';
 import Homepage from './pages/Home';
 import Projects from './pages/Projects';
-import Navbar from './components/Navbar/index';
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Router>
+        <Routes>
+        <Route path="/" element={<Homepage/>} />
+        <Route path="/Homepage" element={<Homepage/>} />
+        <Route path="/Projects" element={<Projects/>} />
+        <Route path="/Contact" element={<Contacts/>} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
